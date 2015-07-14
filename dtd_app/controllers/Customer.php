de9eabdfadd289430c0fc888792853fafe8da61f
Vimal Ghorecha <vimal.ghorecha@gmail.com>
2015-07-14 09:26:22 +0530

    Customer Views Layout Changed

--- dtd_app/controllers/Customer.php
+++ dtd_app/controllers/Customer.php
@@ -5,28 +5,28 @@ class Customer extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->template('customer');
+		$this->load->template('customer/index');
  
 	}
 	public function book_order()
 	{
-		$this->load->template('book_order');              
+		$this->load->template('customer/book_order');              
 	}
 	public function confirm_order()
 	{
-		$this->load->template('cnf-order');              
+		$this->load->template('customer/cnf-order');              
 	}
 	
 	public function profile()
 	{
-		$this->load->template('profile');              
+		$this->load->template('customer/profile');              
 	}
 	public function orders()
 	{
-		$this->load->template('orders');              
+		$this->load->template('customer/orders');              
 	}
 	public function account()
 	{
-		$this->load->template('account');              
+		$this->load->template('customer/account');              
 	}
 }
