af13705a1fd0b2a03d05a43610a3db975e6a647f
Vimal Ghorecha <vimal.ghorecha@gmail.com>
2015-07-14 10:04:12 +0530

    Bower Components Removed

--- dtd_app/views/footer.php
+++ dtd_app/views/footer.php
@@ -2,41 +2,33 @@
     <!-- /#wrapper -->
 
     <!-- jQuery -->
-    <script src="<?=RES_URL;?>bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="<?=RES_URL;?>js/jquery-1.11.1.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?=RES_URL;?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?=RES_URL;?>js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="<?=RES_URL;?>bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="<?=RES_URL;?>js/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
     <script src="<?=RES_URL;?>js/sb-admin-2.js"></script>
-	<script src="<?=RES_URL;?>js/jquery.js"></script>
-	<script src="<?=RES_URL;?>js/jquery.dataTables.js"></script>
-	
-
-	
-	<script src="<?=RES_URL;?>js/jquery-1.11.1.min.js"></script>
-        <script src="<?=RES_URL;?>js/bootstrap-datepicker.js"></script>
-		<script type="text/javascript" language="javascript" src="<?=RES_URL;?>js/dataTables.bootstrap.js"></script>
-		<script type="text/javascript" language="javascript" src="<?=RES_URL;?>js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript">
-            // When the document is ready
-            $(document).ready(function () {
-                
-                $('.datepicker').datepicker({
-                    format: "dd/mm/yyyy"
-                });  
-            
-            });
-        </script>
-		<script type="text/javascript">
-            // When the document is ready
-            $(document).ready(function() {
-    $('#example').DataTable();
-} );
-        </script>
+	<script src="<?=RES_URL;?>js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript" language="javascript" src="<?=RES_URL;?>js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" language="javascript" src="<?=RES_URL;?>js/dataTables.bootstrap.js"></script>
+	<script type="text/javascript">
+		// When the document is ready
+		$(document).ready(function () {
+			$('.datepicker').datepicker({
+				format: "dd/mm/yyyy"
+			});
+		});
+    </script>
+	<script type="text/javascript">
+	// When the document is ready
+		$(document).ready(function() {
+			$('#example').DataTable();
+		});
+	</script>
 		
 </body>
 
